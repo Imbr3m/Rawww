1ad99323e45f42a929645d00da8fe412fa86939d
Rainer Santos <rainertoviel@gmail.com>
2025-08-31 22:35:48 +0400

    added a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './compoments/Hero'
 import About from './compoments/About'
+import Navbar from './compoments/Navbar'
+import Features from './compoments/Features'
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
+      <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   )
 }
