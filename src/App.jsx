6322f8202d764695ab1cc5203bb3aaf1010e64e5
Rainer Santos <rainertoviel@gmail.com>
2025-08-31 18:19:02 +0400

    structured the About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Hero from './compoments/Hero'
+import About from './compoments/About'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
-
-      <section className='z-0 min-h-screen bg-blue-500'></section>
+      <About />
     </main>
   )
 }
